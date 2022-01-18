45949d4342f404fb720c84e7670977d897387f57
HoyaRobotics <hoyarobotics@gmail.com>
2022-01-18 10:24:59 -0500

    commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,11 +4,33 @@ import edu.wpi.first.wpilibj.XboxController.Button;
 
 public final class Constants {
         public static class Controls{
-            //Drive Controls 
+            //Driver Controls 
              public final class GenericHID{
                  public final int DRIVE = Button.kLeftStick.value;
+
+             public class Hand {
+                    public double getTriggerAxis(GenericHID.Hand hand){
+                        return 0;
+
+             
+                         
+                     }
+                }
              }
              public static final int RUN_INTAKE_FWD = Button.kRightBumper.value;
+             public static final int RUN_INTAKE_BWD = Button.kLeftBumper.value;
+             public static final int TOGGLE_INTAKE_RAISED = Button.kX.value;
+             public static final int TOGGLE_CAMERA_VIEW = Button.kY.value;
+             public static final int SHOOT_BALL = Button.kA.value;
+
+
+             // Operator Controls:
+             public static final int ROTATE_TURRET = Button.kRightStick.value;
+             public static final int TOGGLE_LIMELIGHT_ZOOM = Button.kB.value;
+             public static final int INC_RPM_OFFSET = Button.kRightBumper.value;
+             public static final int DEC_RPM_OFFSET = Button.kLeftBumper.value;
+             public static final int CHANGE_TARGET_GOAL= Button.kY.value;
+            
         }
         
         //USB ids
