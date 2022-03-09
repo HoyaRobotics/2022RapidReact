3d16d587b708cc12032be46358c4a27e57394881
HoyaRobotics <hoyarobotics@gmail.com>
2022-03-09 16:37:47 -0500

    Inital set up of Storage.java -Ethan

---
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import frc.robot.Constants;
+import frc.robot.Constants.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+
+public class Storage extends SubsystemBase {
+  /** Creates a new Storage. */
+
+  private CANSparkMax indexMotor;
+  
+  public Storage() {
+    indexMotor = new CANSparkMax(Constants.INDEXER, MotorType.kBrushless);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
