dd76d88ef034ec2b525221a7672122356eccf44e
HoyaRobotics <hoyarobotics@gmail.com>
2022-03-13 14:10:54 -0400

    util fixed

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.util.Logger;
+import frc.robot.utils.Logger;
 
 import frc.robot.Constants.*;
 import frc.robot.Constants;
