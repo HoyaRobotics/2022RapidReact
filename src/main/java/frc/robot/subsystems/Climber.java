741ae87d10274c802cc384b31fb795eda40774e6
Hoya Robotics Team 4152 <35349616+HoyaRobotics@users.noreply.github.com>
2022-03-13 09:13:21 -0400

    Climber subsytem
    Added default code.

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -5,15 +5,47 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.*;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
+  private CANSparkMax LeftMotor;
+  private CANSparkMax RightMotor;
+  private final Solenoid LeftSolenoid  = new Solenoid(PneumaticsModuleType.REVPH, Constants.CLIMBER_LEFT_PCM);
+  private final Solenoid RightSolenoid  = new Solenoid(PneumaticsModuleType.REVPH, Constants.CLIMBER_RIGHT_PCM);
+  private boolean angled = true;//flag to ensure intake starts raised.
+  
+  
   public Climber() {
-    System.out.println("Climber constructor called");
+    this.LeftMotor = new CANSparkMax(Constants.CLIMBER_VERTICAL_L, MotorType.kBrushless);
+    this.RightMotor = new CANSparkMax(Constants.CLIMBER_VERTICAL_R, MotorType.kBrushless);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  
+  public void setClimberMotor(double speed){
+    
+    this.LeftMotor.set(speed);
+    this.RightMotor.set(speed);
+  }
+  /*Used to angle the arms*/
+  public void setAngled(boolean angled){
+    if(this.angled != angled)
+      this.angled = angled;
+
+    LeftSolenoid.set(!this.angled);
+    RightSolenoid.set(!this.angled);
+  }
+
+  /*Changes the intake status (raises if lowered, lowers if raised)*/
+  public void toggleAngled(){
+    setAngled(!angled);
   }
 }
